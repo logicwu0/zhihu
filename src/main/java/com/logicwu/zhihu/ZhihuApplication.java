888f2ccc91742207632053bf0379c4da77f4dca8
wutianlong <wutianlong@dtbpoint.com>
2018-03-19 15:27:27 +0800

    add：maven创建项目

---
+++ src/main/java/com/logicwu/zhihu/ZhihuApplication.java
@@ -0,0 +1,18 @@
+package com.logicwu.zhihu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ZhihuApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ZhihuApplication.class);
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhihuApplication.class, args);
+	}
+}
