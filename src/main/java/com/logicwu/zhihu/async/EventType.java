2789737d16b31f054fcd0ea9a06a8ace1d81a502
logicwu0 <605524858@qq.com>
2018-04-08 14:16:17 +0800

    add:handle

---
+++ src/main/java/com/logicwu/zhihu/async/EventType.java
@@ -0,0 +1,22 @@
+package com.logicwu.zhihu.async;
+
+public enum EventType {
+    LIKE(0),
+    COMMENT(1),
+    LOGIN(2),
+    MAIL(3),
+    FOLLOW(4),
+    UNFOLLOW(5),
+    ADD_QUESTION(6);
+
+    private int value;
+
+    EventType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
