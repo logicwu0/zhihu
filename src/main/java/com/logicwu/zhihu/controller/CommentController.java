2b586b183d6edb5dedb874b568ed18a0a70bf3fc
logicwu0 <605524858@qq.com>
2018-04-25 15:14:11 +0800

    mod

--- src/main/java/com/logicwu/zhihu/controller/CommentController.java
+++ src/main/java/com/logicwu/zhihu/controller/CommentController.java
@@ -1,16 +1,27 @@
 package com.logicwu.zhihu.controller;
 
 
+import com.logicwu.zhihu.model.News;
 import com.logicwu.zhihu.model.common.ResultData;
+import com.logicwu.zhihu.service.NewsService;
+import com.logicwu.zhihu.util.ZhihuUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import redis.clients.jedis.Jedis;
 
+import java.util.Date;
+
 @Controller
 public class CommentController {
+
+    @Autowired
+    NewsService newsService;
 
     private static final Logger logger = LoggerFactory.getLogger(CommentController.class);
 
@@ -24,6 +35,33 @@ public class CommentController {
         resultData.setData(jedis);
 
         return resultData;
+    }
+
+    @RequestMapping(path = {"/user/addNews/"}, method = {RequestMethod.POST})
+    @ResponseBody
+    public String addNews(@RequestParam("image") String image,
+                          @RequestParam("title") String title,
+                          @RequestParam("link") String link){
+        try {
+            News news = new News();
+            news.setCreatedDate(new Date());
+            news.setTitle(title);
+            news.setLink(link);
+            news.setImage(image);
+            //if (hostHolder != null){
+            //news.setUserId(hostHolder.getUsers().getId());
+            //}
+            //else {
+                //设置一个匿名用户
+               news.setUserId(2);
+            //}
+            newsService.addNews(news);
+            return ZhihuUtil.getJSONString(0);
+        } catch (Exception e) {
+            logger.error("添加资讯失败", e.getMessage());
+            return ZhihuUtil.getJSONString(1, "发布失败");
+        }
+
     }
 
 }
---
+++ src/main/java/com/logicwu/zhihu/service/NewsService.java
@@ -0,0 +1,45 @@
+package com.logicwu.zhihu.service;
+
+
+import com.logicwu.zhihu.dao.NewsDAO;
+import com.logicwu.zhihu.model.News;
+import com.logicwu.zhihu.util.ZhihuUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class NewsService {
+    @Autowired
+    private NewsDAO newsDAO;
+
+    public List<News> getLatestNews(int userId, int offset, int limit) {
+        return newsDAO.selectByUserIdAndOffset(userId, offset, limit);
+    }
+
+    public int addNews(News news) {
+        newsDAO.addNews(news);
+        return news.getId();
+    }
+
+    public News getById(int newsId) {
+        return newsDAO.getById(newsId);
+    }
+
+
+    public int updateCommentCount(int id, int count) {
+        return newsDAO.updateCommentCount(id, count);
+    }
+
+    public int updateLikeCount(int id, int count) {
+        return newsDAO.updateLikeCount(id, count);
+    }
+}
+
